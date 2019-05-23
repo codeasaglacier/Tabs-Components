53e984401838c8dad5b622f8f2daacc0ab42827a
codeasaglacier <remingtonshock@gmail.com>
2019-05-23 14:46:16 -0800

    built part of tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,13 +2,14 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    console.log(this.itemElement, this.data)
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
@@ -59,4 +60,7 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+const links = document.querySelectorAll('.tabs-link').forEach(tabsLink => {
+  new TabLink(tabsLink)
+});
+
